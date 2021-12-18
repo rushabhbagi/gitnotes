c9603aa1a8e19e78b2789bb77d7daaf76106d82d
Dhiraj D <itsdhirajdi@gmail.com>
2021-12-18 13:15:23 +0530

    all dept added

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,7 @@ function scrollHeader() {
 window.addEventListener("scroll", scrollHeader);
 
 // mixitup js
-var mixer = mixitup(document.getElementsByClassName("gift__container"));
+var mixer = mixitup(document.getElementsByClassName("semester__container"));
 
 // link active semester
 const linkSemester = document.querySelectorAll(".nav_button");
@@ -51,3 +51,54 @@ function activeSemester() {
   }
 }
 linkSemester.forEach((l) => l.addEventListener("click", activeSemester));
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true
+});
+
+sr.reveal(`.home__img, .new__container, .footer__container`);
+sr.reveal(`.home__data`, { delay: 500 });
+sr.reveal(`.giving__content, .semester__card`, { interval: 100 });
+sr.reveal(`.celebrate__data, .message__form, .footer__img1`, {
+  origin: "left",
+});
+sr.reveal(`.celebrate__img, .message__img, .footer__img2`, { origin: "right" });
