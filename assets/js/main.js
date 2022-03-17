8d417bfa47234d337c373b8df85226be84fad284
Dhiraj D <itsdhirajdi@gmail.com>
2022-03-17 13:49:35 +0530

    ece ads link

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,28 +54,28 @@ linkSemester.forEach((l) => l.addEventListener("click", activeSemester));
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll("section[id]");
+// const sections = document.querySelectorAll("section[id]");
 
-function scrollActive() {
-  const scrollY = window.pageYOffset;
+// function scrollActive() {
+//   const scrollY = window.pageYOffset;
 
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight,
-      sectionTop = current.offsetTop - 58,
-      sectionId = current.getAttribute("id");
+//   sections.forEach((current) => {
+//     const sectionHeight = current.offsetHeight,
+//       sectionTop = current.offsetTop - 58,
+//       sectionId = current.getAttribute("id");
 
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.add("active-link");
-    } else {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.remove("active-link");
-    }
-  });
-}
-window.addEventListener("scroll", scrollActive);
+//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//       document
+//         .querySelector(".nav__menu a[href*=" + sectionId + "]")
+//         .classList.add("active-link");
+//     } else {
+//       document
+//         .querySelector(".nav__menu a[href*=" + sectionId + "]")
+//         .classList.remove("active-link");
+//     }
+//   });
+// }
+// window.addEventListener("scroll", scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/
 function scrollUp() {
@@ -91,7 +91,7 @@ const sr = ScrollReveal({
   origin: "top",
   distance: "60px",
   duration: 2500,
-  delay: 400,
+  delay: 150,
   // reset: true
 });
 
